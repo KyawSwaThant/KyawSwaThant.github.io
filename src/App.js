baf6cd656fa0276b23ec34bc336d073b2b1f51aa
Kyaw Swar Thant <kyawswar.thant25@gmail.com>
2021-05-12 13:36:59 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-5">
+              <img className="rounded-circle" src="/img/profile.jpeg" alt="profile.jpeg" width="300" height="300" />
+            </div>
+            <div className="col-md">
+              <div className="card">
+                <h3 className="text-center">Information</h3>
+                <div className="card-body">
+                  <ul className="list-unstyled">
+                    <li>Student at University of Computer Studies Mandalay.</li>
+                    <li>Beginner Web Developer.</li>
+                    <li>Live in Mandalay.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                    <ul className="list-unstyled">
+                        <li><i className="fa fa-phone fa-lg"></i>Phone: +959772709300</li>
+                        <li><i className="fa fa-envelope fa-lg"></i>Email: admin@kyawswarthant.me</li>
+                    </ul>
+                </div>
+                <div className="col-md">
+                    <a className="btn btn-social-icon btn-facebook" href="https://google.com"><i className="fa fa-facebook fa-lg"></i></a>
+                    <a className="btn btn-social-icon btn-gmail" href="https://google.com"><i className="fa fa-envelope fa-lg"></i></a>
+                    <a className="btn btn-social-icon btn-twitter" href="https://google.com"><i className="fa fa-twitter fa-lg"></i></a>
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
   );
 }
